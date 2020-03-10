6def0752833fcdb14bc01139700342fadf025d52
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2020-03-10 09:54:03 +0530

    subject added in shopping service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingService } from './shopping.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, DoCheck {
+export class ShoppingListComponent implements OnInit, DoCheck, OnDestroy {
   ingredients: Ingredient[];
+  shoppingServiceSubscription:Subscription;
   constructor(private shoppingService:ShoppingService) { }
 
   ngOnInit(): void {
     this.ingredients=this.shoppingService.getIngredients();
-    this.shoppingService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
+    this.shoppingServiceSubscription=this.shoppingService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
       this.ingredients=ingredients;
     })
-
   }
+
+  ngOnDestroy(){
+    this.shoppingServiceSubscription.unsubscribe();
+  }
+
   ngDoCheck(){
   //   this.ingredients=this.shoppingService.getIngredients();
 
--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingService {
-  ingredientsChanged= new EventEmitter<Ingredient[]>();
+  // ingredientsChanged= new EventEmitter<Ingredient[]>();
+  ingredientsChanged= new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -18,11 +20,12 @@ export class ShoppingService {
 
   onIngredientAdded(ingredient:Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    // this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 
   onIngredientsAdded(ingredients:Ingredient[]){
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
   }  
 }
